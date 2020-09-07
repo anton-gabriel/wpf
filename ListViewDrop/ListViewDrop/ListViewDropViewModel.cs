e0b76fec1c63dcca133e8e602085fad61a4cbce6
anton-gabriel <anton.gabi1998@gmail.com>
2020-09-07 14:46:23 +0300

    Add ListViewDrop solution

---
+++ ListViewDrop/ListViewDrop/ListViewDropViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ListViewDrop
+{
+    internal sealed class ListViewDropViewModel
+        : NotifyPropertyChanged
+    {
+        #region Constructors
+        public ListViewDropViewModel()
+        {
+            Paths = new ObservableCollection<string>()
+            {
+                "Path1",
+                "Path2"
+            };
+        }
+        #endregion
+
+        #region Private fields
+
+        #endregion
+
+        #region Properties
+        public ObservableCollection<string> Paths { get; private set; }
+        #endregion
+
+        #region Commands
+
+        #endregion
+    }
+}
---
+++ ListViewDrop/ListViewDrop/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ListViewDrop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ListView_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source is ListView source)
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    DragDrop.DoDragDrop(source, new DataObject(format: typeof(IList), data: source.SelectedItems), DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void ListView_Drop(object sender, DragEventArgs args)
+        {
+            if (args.Data.GetDataPresent(typeof(IList)))
+            {
+                var selection = args.Data.GetData(typeof(IList)) as IList;
+                if (args.Source is ListView source)
+                {
+                    selection?.OfType<string>().ToList().ForEach(item => source.Items.Add(item));
+                }
+            }
+        }
+
+        private void GridViewColumn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
