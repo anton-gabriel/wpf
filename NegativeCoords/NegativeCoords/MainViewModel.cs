0a33e4335830646fdc1b5c2e36fa593cf2e8d784
anton-gabriel <anton.gabi1998@gmail.com>
2020-09-08 22:14:03 +0300

    Update Selection tool solution

---
+++ NegativeCoords/NegativeCoords/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegativeCoords
+{
+
+    internal sealed class MainViewModel
+        : INotifyPropertyChanged
+    {
+        #region Constructors
+        public MainViewModel()
+        {
+            SelectionViewModel = new SelectionViewModel();
+        }
+        #endregion
+
+        #region Private fields
+
+        #endregion
+
+        #region Properties
+        public SelectionViewModel SelectionViewModel { get; private set; }
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string name = default)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
